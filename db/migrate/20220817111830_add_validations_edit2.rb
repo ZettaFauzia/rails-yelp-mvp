803fc819ad0bb80b8ae253d439df225ff1f63c3e
LucaFauzia <luca.fauzia@live.com>
2022-08-17 15:59:34 +0100

    message

---
+++ db/migrate/20220817111830_add_validations_edit2.rb
@@ -0,0 +1,4 @@
+class AddValidationsEdit2 < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20220817112133_add_validations_edit3.rb
@@ -0,0 +1,4 @@
+class AddValidationsEdit3 < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
